db0b9358499af3fc1baca11ed8892dbd1568fe69
4-R-Forum <8607712+4-R-Forum@users.noreply.github.com>
2023-10-07 19:22:56 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+let cmd = ""
+let roll = 0
+let pitch = 0
+let button = 0
+serial.redirectToUSB()
+let count = 0
+basic.showIcon(IconNames.SmallDiamond)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.AB)) {
+        basic.showIcon(IconNames.Square)
+        game.pause()
+    }
+    count = count + 1
+    if (input.buttonIsPressed(Button.A)) {
+        button = 1
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        button = -1
+    }
+    pitch = input.rotation(Rotation.Pitch)
+    roll = input.rotation(Rotation.Roll)
+    cmd = "" + convertToText(count) + " , " + convertToText(pitch) + " , " + convertToText(roll) + ", " + convertToText(button)
+    serial.writeLine(cmd)
+    basic.pause(1000)
+})
